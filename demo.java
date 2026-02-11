ac5fccf97a8de6d7aa9de86f95184f197c751d9d
Harsha Hn <you@example.com>
2026-02-11 15:49:34 +0530

    initial committ

--- demo.java
+++ demo.java
@@ -11,3 +11,30 @@ public class demo {
     }
     
 }
+
+class demo1 {
+    public static void main(String [] args){
+        String a= new String("Apple");
+        String b= new String("Apple");
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+    }
+}
+
+class array {
+    public static void main(String [] args) {
+        int[] arr = {1,2,3,4,5};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+class array2 {
+    public static void main(String [] args) {
+        int[] arr = {1,2,3,4,5};
+        for(int num:arr){
+            System.out.println(num);
+        }
+    }
+}
